7224535720c9fda94eaa1308ec8801ec51007cae
iurilugao01 <128332733+iurilugao01@users.noreply.github.com>
2024-07-19 16:58:35 -0300

    Add files via upload

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
 import random
+
+# Função lambda para rolar um dado
 roll = lambda dice: random.randint(1, dice)
 
-
-class Create_token:
+class CreateToken:
     def __init__(self, name, life, damage_dice, damage_bonus, attack_bonus, armor, cd):
         self.name = name
         self.life = life
@@ -14,26 +15,33 @@ class Create_token:
         self.cd = cd
 
     def roll_damage(self, target):
-        damage = roll(20) + self.damage_bonus - target.armor
+        damage = roll(self.damage_dice) + self.damage_bonus - target.armor
         if target.blocking:
             damage -= 5
-        return damage
+        return max(damage, 0)  # Evita dano negativo
 
- def attack(self, target):
+    def attack(self, target):
         hit = target.cd <= roll(20) + self.attack_bonus
         if hit:
-            total_damage = roll(self.damage_dice) - target.armor
+            total_damage = self.roll_damage(target)
             target.life -= total_damage
-
-            print(f'o ataque acertou! causou: {total_damage} de dano')
+            print(f'O ataque acertou! Causou: {total_damage} de dano.')
         else:
-            print('o ataque falhou!')
+            print('O ataque falhou!')
 
     def block(self):
         self.blocking = True
 
+# Exemplo de uso:
+token1 = CreateToken("Guerreiro", 100, 8, 3, 5, 2, 15)
+token2 = CreateToken("Inimigo", 50, 6, 2, 4, 1, 12)
+
+token1.attack(token2)
+token2.block()
+token1.attack(token2)
+token2.blocking = False  # Reseta o bloqueio após um turno (exemplo)
 
 
-
+teste = {}
 
 
